018c2b0d823559ab05d7de3ba26128130f3e2c0e
joeychun <joey.s.chun@gmail.com>
2024-04-30 15:02:44 +0900

    deleted skeleton files

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { get, post } from "../utilities";
 import NotFound from "./pages/NotFound";
-import Skeleton from "./pages/Skeleton";
 import { socket } from "../client-socket";
 import User from "../../../shared/User";
 import "../utilities.css";
